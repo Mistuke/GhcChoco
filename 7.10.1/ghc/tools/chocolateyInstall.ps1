080908637d69d4f24b1f72a38f3b430d4b027c42
Tamar Christina <tamar@zhox.com>
2015-06-18 23:28:50 +0200

    Choco: got install working, uninstall needs work

--- 7.10.1/ghc/tools/chocolateyInstall.ps1
+++ 7.10.1/ghc/tools/chocolateyInstall.ps1
@@ -16,7 +16,7 @@ try {
   # if removing $url64, please remove from here
   # despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
   Install-ChocolateyZipPackage "$packageName" "$url" (Join-Path $env:chocolateyPackageFolder tmp) "$url64"
-  Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder ("tmp\" + $packageName + "Install")) -destination "$packageFullName"
+  Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder (Join-Path tmp ($packageName + "Install"))) -destination "$installDir"
 
   Write-Host "Adding `'$binPackageDir`' to the path and the current shell path"
   Install-ChocolateyPath "$binPackageDir"
@@ -24,6 +24,6 @@ try {
   
   Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw $_.Exception
   throw
 }
