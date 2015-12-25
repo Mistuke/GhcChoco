16fad274fa7aad5a721f5a4b44bda5ab96456eba
Tamar Christina <tamar@zhox.com>
2015-12-25 14:07:23 +0100

    Choco: Added GHC 7.10.3

---
+++ 7.10.3/ghc/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$version     = '7.10.3' # package version
+$packageName = 'ghc' # arbitrary name for the package, used in messages
+$url         = 'http://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-i386-unknown-mingw32.tar.xz' # download url
+$url64       = 'http://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-x86_64-unknown-mingw32.tar.xz' # 64bit URL here or remove - if installer decides, then use $url
+
+$thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. ($thisScript +  '.\chocolateyInstall-Template.ps1')
