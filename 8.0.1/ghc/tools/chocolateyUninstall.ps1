0f9758c44f21371477570c5dba39de2cb70166c5
Tamar Christina <tamar@zhox.com>
2016-05-21 19:06:03 +0200

    Choco: Added GHC 8.0.1 final

---
+++ 8.0.1/ghc/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$version     = '8.0.1' # package version
+$packageName = 'ghc' # arbitrary name for the package, used in messages
+
+$thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. ($thisScript +  '.\chocolateyUninstall-Template.ps1')
