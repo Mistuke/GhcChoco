ea984c0450d47ddd158f08d015615c8a483db23f
Tamar Christina <tamar@zhox.com>
2016-06-01 10:51:33 +0200

    Ghc: Updated wrapper scipts to alternate approach

--- templates/chocolateyInstall-Template.ps1
+++ templates/chocolateyInstall-Template.ps1
@@ -1,19 +1,32 @@
 ﻿try {
-  #$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   ### For BinRoot, use the following instead ###
-  $binRoot         = Get-BinRoot
-  $installDir      = Join-Path $binRoot "$packageName"
-  $packageFullName = Join-Path $installDir ($packageName + '-' + $version)
+  $binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
   $binPackageDir   = Join-Path $packageFullName "bin"
-  
-  # if removing $url64, please remove from here
-  # despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
-  Install-ChocolateyZipPackage "$packageName" "$url" (Join-Path $env:chocolateyPackageFolder tmp) "$url64"
-  Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder (Join-Path tmp ($packageName + "Install"))) -destination "$installDir"
+  $osBitness       = Get-ProcessorBits
+
+  $tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+  $tarFile = $packageName + "Install"
+  $tarPath = Join-Path $tmpPath $tarFile
+  $tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+  Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath -Url $url -Url64bit $url64
+  Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+  Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+  rm $tmpFile # Clean up temporary file
 
   Write-Host "Adding `'$binPackageDir`' to the path and the current shell path"
   Install-ChocolateyPath "$binPackageDir"
   
+  # Blocking shims
+  $files = get-childitem $binRoot -include *.exe -recurse
+
+  foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+  }
+
+
   # Reload the session to update path
   Write-Host "$packageName $version has been installed. You may need to restart the console to use it."
 } catch {
