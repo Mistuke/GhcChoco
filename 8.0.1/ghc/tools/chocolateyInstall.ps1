6bb798dd4ebec1228da5057c3270adc4181fb1c8
Tamar Christina <tamar@zhox.com>
2016-06-10 09:15:36 +0200

    Ghc: modified 8.0.1 w.r.t. review

--- 8.0.1/ghc/tools/chocolateyInstall.ps1
+++ 8.0.1/ghc/tools/chocolateyInstall.ps1
@@ -3,5 +3,26 @@ $packageName = 'ghc'
 $url         = 'http://downloads.haskell.org/~ghc/8.0.1/ghc-8.0.1-i386-unknown-mingw32.tar.xz'
 $url64       = 'http://downloads.haskell.org/~ghc/8.0.1/ghc-8.0.1-x86_64-unknown-mingw32.tar.xz'
 
-$thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-. ($thisScript +  '.\chocolateyInstall-Template.ps1')
+$binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+$tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+$tarFile = $packageName + "Install"
+$tarPath = Join-Path $tmpPath $tarFile
+$tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath -Url $url -Url64bit $url64
+Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+rm $tmpFile # Clean up temporary file
+
+Install-ChocolateyPath "$binPackageDir"
+
+Write-Host "Hiding shims for `'$binRoot`'."
+$files = get-childitem $binRoot -include *.exe -recurse
+
+foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
