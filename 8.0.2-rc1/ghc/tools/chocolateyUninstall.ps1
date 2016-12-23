e27d2e3bba92167cac1f820ccd45abcbc9a17f8e
Tamar Christina <tamar@zhox.com>
2016-12-23 13:31:49 +0000

    GhcChoco: Apply chocolatey maintainer rules.

--- 8.0.2-rc1/ghc/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$version     = '8.0.1.20161117'
-$packageName = 'ghc'
-
-$thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-. ($thisScript +  '.\chocolateyUninstall-Template.ps1')
