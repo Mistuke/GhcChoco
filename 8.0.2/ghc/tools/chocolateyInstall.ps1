50f3c569c3b81f84410691026d4c6e886820c36c
Tamar Christina <tamar@zhox.com>
2017-02-02 20:23:57 +0000

    Add checksum and use https.

--- 8.0.2/ghc/tools/chocolateyInstall.ps1
+++ 8.0.2/ghc/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$version     = '8.0.2'
+$version     = '8.0.2'
 $packageName = 'ghc'
-$url         = 'http://downloads.haskell.org/~ghc/8.0.2/ghc-8.0.2-i386-unknown-mingw32.tar.xz'
-$url64       = 'http://downloads.haskell.org/~ghc/8.0.2/ghc-8.0.2-x86_64-unknown-mingw32.tar.xz'
+$url         = 'https://downloads.haskell.org/~ghc/8.0.2/ghc-8.0.2-i386-unknown-mingw32.tar.xz'
+$url64       = 'https://downloads.haskell.org/~ghc/8.0.2/ghc-8.0.2-x86_64-unknown-mingw32.tar.xz'
 
 $binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
@@ -12,7 +12,7 @@ $tarFile = $packageName + "Install"
 $tarPath = Join-Path $tmpPath $tarFile
 $tmpFile = Join-Path $binRoot ($tarFile + "~")
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath -Url $url -Url64bit $url64
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath -Url $url -ChecksumType sha256 -Checksum 328B7D4395F4802118054E03C099218B99D0EBB141326964E1C3E591924A1F31 -Url64bit $url64  -ChecksumType64 sha256 -Checksum64 8C42C1F4AF995205B9816A1E97E2752FE758544C1F5FE77958CDCD319C9C2D53
 Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
 Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
 rm $tmpFile # Clean up temporary file
