955a30d1c13523e932405e3db55b8e10c56c8b92
Tamar Christina <tamar@zhox.com>
2018-07-17 21:41:56 +0100

    GhcChoco: add 8.6.1 alpha 2

---
+++ 8.6.1-alpha2/ghc/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$version     = 'ghc-8.6.0.20180714'
+$packageName = 'ghc'
+$url         = 'https://downloads.haskell.org/~ghc/8.6.1-alpha2/ghc-8.6.0.20180714-i386-unknown-mingw32.tar.xz'
+$url64       = 'https://downloads.haskell.org/~ghc/8.6.1-alpha2/ghc-8.6.0.20180714-x86_64-unknown-mingw32.tar.xz'
+
+$binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+$tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+$tarFile = $packageName + "Install"
+$tarPath = Join-Path $tmpPath $tarFile
+$tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath `
+    -Url $url -ChecksumType sha256 -Checksum 4355eeff305064cd002abc95f44a7a7cbb84c7f3ff77fe77c3aec4b2e773f4e7 `
+    -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 a9704c4ef91a4301a79de428455e283a4ac40dd30c4ff6635c766896153bcc07 `
+Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+rm $tmpFile # Clean up temporary file
+
+Install-ChocolateyPath "$binPackageDir"
+
+Write-Host "Hiding shims for `'$binRoot`'."
+$files = get-childitem $binRoot -include *.exe -recurse
+
+foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
