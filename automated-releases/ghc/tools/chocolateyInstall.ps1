dad416267aa963d113160674ccb2b14633d2b7a3
Tamar Christina <tamar@zhox.com>
2019-03-24 17:26:51 +0000

    Add template for automatic CI releases

---
+++ automated-releases/ghc/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$version     = '%build.version%'
+$packageName = 'ghc'
+$url         = '%deploy.url.32bit%'
+$url64       = '%deploy.url.64bit%'
+
+$binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+$tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+$tarFile = $packageName + "Install"
+$tarPath = Join-Path $tmpPath $tarFile
+$tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+Get-ChocolateyWebFile `
+  -PackageName $packageName `
+  -FileFullPath $tarPath `
+  -Url $url -ChecksumType sha256 -Checksum '%deploy.sha256.32bit%' `
+  -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 '%deploy.sha256.64bit%'
+Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+rm $tmpFile # Clean up temporary file
+
+Install-ChocolateyPath "$binPackageDir"
+
+Write-Host "Hiding shims for `'$binRoot`'."
+$files = get-childitem $binRoot -include *.exe -recurse
+
+foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
