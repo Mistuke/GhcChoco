51e82620524cf8697821580a5cc38b2aca7856df
Tamar Christina <tamar@zhox.com>
2019-03-24 17:02:30 +0000

    Add 8.6.4 32bit GHC self hosted.'

--- 8.6.4/ghc/tools/chocolateyInstall.ps1
+++ 8.6.4/ghc/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$version     = '8.6.4'
 $packageName = 'ghc'
-$url         = 'https://downloads.haskell.org/~ghc/8.6.4/ghc-8.6.4-i386-unknown-mingw32.tar.xz'
+$url         = 'https://mistuke.blob.core.windows.net/binaries/unofficial/~ghc/8.6.4/ghc-8.6.4-i386-unknown-mingw32.tar.xz'
 $url64       = 'https://downloads.haskell.org/~ghc/8.6.4/ghc-8.6.4-x86_64-unknown-mingw32.tar.xz'
 
 $binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
@@ -13,7 +13,7 @@ $tarPath = Join-Path $tmpPath $tarFile
 $tmpFile = Join-Path $binRoot ($tarFile + "~")
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath `
-    -Url $url -ChecksumType sha256 -Checksum <missing> `
+    -Url $url -ChecksumType sha256 -Checksum 384ff629576627d3c24a73c52e404f8ab72c65ccf3e22f74268a5eae7aa5ed61 `
     -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 e8d021b7a90772fc559862079da20538498d991956d7557b468ca19ddda22a08
 Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
 Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
