ea16b5ecc3fab25c8f3bdde56d106a9c0b6aafd4
Tamar Christina <tamar@zhox.com>
2019-08-24 22:31:47 +0100

    add refreshenv for travis CI

--- nightly/ghc/tools/chocolateyInstall.ps1
+++ nightly/ghc/tools/chocolateyInstall.ps1
@@ -30,3 +30,33 @@ foreach ($file in $files) {
     #generate an ignore file
     New-Item "$file.ignore" -type file -force | Out-Null
 }
+
+if (($null -ne $Env:TRAVIS) -and ("" -ne $Env:TRAVIS)) {
+  Write-Host "Configuring TRAVIS aliases."
+  # We need to fix up refreshenv for Travis
+  $rc = $Env:HOME + "/.bashrc"
+
+  Add-Content $rc "function refreshenv`n
+  {`n
+    powershell -NonInteractive - <<\EOF`n
+  Import-Module `"$env:ChocolateyInstall\helpers\chocolateyProfile.psm1`"`n
+  Update-SessionEnvironment`n
+  # Round brackets in variable names cause problems with bash`n
+  Get-ChildItem env:* | %{`n
+    if (!($_.Name.Contains('('))) {`n
+      $value = $_.Value`n
+      if ($_.Name -eq 'PATH') {`n
+        $value = $value -replace ';',':'`n
+      }`n
+      Write-Output (`"export `" + $_.Name + `"='`" + $value + `"'`")`n
+    }`n
+  } | Out-File -Encoding ascii $env:TEMP\refreshenv.sh`n
+  EOF`n
+  `n
+    source `"$TEMP/refreshenv.sh`"`n
+  }`n
+  `n
+  alias RefreshEnv=refreshenv`n"
+
+  Write-Host "Ok, updated ~/.bashrc, source to use refreshenv."
+}
