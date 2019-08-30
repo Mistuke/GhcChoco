d6e0628ef5679af2f841e1518c926061680ef09d
Tamar Christina <tamar@zhox.com>
2019-08-30 07:14:12 +0100

    have 8.8.1 on 32 bit install 8.6.5 instead of crashing. prompt users to go moan at GHC HQ

--- 8.8.1/ghc/tools/chocolateyInstall.ps1
+++ 8.8.1/ghc/tools/chocolateyInstall.ps1
@@ -1,7 +1,26 @@
 ﻿$version     = '8.8.1'
 $packageName = 'ghc'
-$url         = 'https://downloads.haskell.org/~ghc/8.8.1/ghc-8.8.1-i386-unknown-mingw32.tar.xz'
+$url         = 'https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-i386-unknown-mingw32.tar.xz'
 $url64       = 'https://downloads.haskell.org/~ghc/8.8.1/ghc-8.8.1-x86_64-unknown-mingw32.tar.xz'
+
+$is32 = (Get-OSArchitectureWidth 32)  -or $env:chocolateyForceX86 -eq 'true'
+
+if($is32)
+  {
+      Write-Host "#     # ####### ####### ###  #####  #######"
+      Write-Host "##    # #     #    #     #  #     # #      "
+      Write-Host "# #   # #     #    #     #  #       #      "
+      Write-Host "#  #  # #     #    #     #  #       #####  "
+      Write-Host "#   # # #     #    #     #  #       #      "
+      Write-Host "#    ## #     #    #     #  #     # #      "
+      Write-Host "#     # #######    #    ###  #####  #######"
+      Write-Host ""
+      Write-Host " GHC HQ have not produced a 32 bit binary for Windows again."
+      Write-Host " I will now install GHC 8.6.5 instead.  If you find this"
+      Write-Host " unacceptable please let them know that 32-bits is important"
+      Write-Host " by creating an issue at https://gitlab.haskell.org/ghc/ghc/issues"
+      Write-Host ""
+  }
 
 $binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
@@ -13,7 +32,7 @@ $tarPath = Join-Path $tmpPath $tarFile
 $tmpFile = Join-Path $binRoot ($tarFile + "~")
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath `
-    -Url $url -ChecksumType sha256 -Checksum <missing> `
+    -Url $url -ChecksumType sha256 -Checksum 2a8fb73080ed4335f7a172fe6cf9da1a2faa51fdb72817c50088292f497fc57a `
     -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 29e56e6af38017a5a76b2b6995a39d3988fa58131e4b55b62dd317ba7186ac9b
 Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
 Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
