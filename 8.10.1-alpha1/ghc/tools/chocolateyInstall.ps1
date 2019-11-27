f32cd25377a02a8dbf4e2586c541879ce90bd578
Tamar Christina <tamar@zhox.com>
2019-11-27 07:23:06 +0000

    Update hashes

--- 8.10.1-alpha1/ghc/tools/chocolateyInstall.ps1
+++ 8.10.1-alpha1/ghc/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $tarFile = $packageName + "Install"
 $tarPath = Join-Path $tmpPath $tarFile
 $tmpFile = Join-Path $binRoot ($tarFile + "~")
 
-Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 1b5ff7995a07e7251b26b3c7dadba57906b7e6d236d0e94c4e92d9b5b8ee3300
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 f75d28793fb093df9de1329be62604a980150b159909d608b826f1b29bb369cc
 Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
 Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
 rm $tmpFile # Clean up temporary file
@@ -25,4 +25,37 @@ $files = get-childitem $binRoot -include *.exe -recurse
 foreach ($file in $files) {
     #generate an ignore file
     New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+if (($null -ne $Env:TRAVIS) -and ("" -ne $Env:TRAVIS)) {
+  Write-Host "Configuring Travis aliases."
+  # We need to fix up refreshenv for Travis
+  $rc = $Env:HOME + "/.bashrc"
+
+  Add-Content $rc "function refreshenv
+{
+  powershell -NonInteractive - <<\EOF
+Import-Module -Force `"`$env:ChocolateyInstall\helpers\chocolateyProfile.psm1`"
+`$pref = `$ErrorActionPreference
+`$ErrorActionPreference = 'SilentlyContinue'
+Update-SessionEnvironment
+`$ErrorActionPreference = `$pref
+# Round brackets in variable names cause problems with bash
+Get-ChildItem env:* | %{
+  if (!(`$_.Name.Contains('('))) {
+    `$value = `$_.Value
+    if (`$_.Name -eq 'PATH') {
+      `$value = `$value -replace ';',':'
+    }
+    Write-Output (`"export `" + `$_.Name + `"='`" + `$value + `"'`")
+  }
+} | Out-File -Encoding ascii $env:TEMP\refreshenv.sh
+EOF
+
+  source `"$env:TEMP/refreshenv.sh`"
+}
+
+alias RefreshEnv=refreshenv"
+
+  Write-Host "Ok, updated ~/.bashrc, source to use refreshenv."
 }
