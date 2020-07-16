8ac064b7838766b10cac36bd8384ed7b7e6473ea
Tamar Christina <tamar@zhox.com>
2020-07-16 23:12:40 +0100

    Add GHC 8.8.4

---
+++ 8.8.4/ghc/tools/chocolateyInstall.ps1
@@ -0,0 +1,86 @@
+﻿$version     = '8.8.4'
+$packageName = 'ghc'
+$url         = 'https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-i386-unknown-mingw32.tar.xz'
+$url64       = 'https://downloads.haskell.org/~ghc/8.8.3/ghc-8.8.4-x86_64-unknown-mingw32.tar.xz'
+
+$is32 = (Get-OSArchitectureWidth 32)  -or $env:chocolateyForceX86 -eq 'true'
+
+if($is32)
+  {
+      Write-Host "#     # ####### ####### ###  #####  #######"
+      Write-Host "##    # #     #    #     #  #     # #      "
+      Write-Host "# #   # #     #    #     #  #       #      "
+      Write-Host "#  #  # #     #    #     #  #       #####  "
+      Write-Host "#   # # #     #    #     #  #       #      "
+      Write-Host "#    ## #     #    #     #  #     # #      "
+      Write-Host "#     # #######    #    ###  #####  #######"
+      Write-Host ""
+      Write-Host " GHC HQ have not produced a 32 bit binary for Windows again."
+      Write-Host " I will now install GHC 8.6.5 instead."
+      Write-Host ""
+      # rewrite the version to 8.6,5 so installer works
+      $version = '8.6.5'
+  }
+
+$binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+$tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+$tarFile = $packageName + "Install"
+$tarPath = Join-Path $tmpPath $tarFile
+$tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath `
+    -Url $url -ChecksumType sha256 -Checksum 2a8fb73080ed4335f7a172fe6cf9da1a2faa51fdb72817c50088292f497fc57a `
+    -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 e22586762af0911c06e8140f1792e3ca381a3a482a20d67b9054883038b3a422
+Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+rm $tmpFile # Clean up temporary file
+
+Install-ChocolateyPath "$binPackageDir"
+
+Write-Host "Hiding shims for `'$binRoot`'."
+$files = get-childitem $binRoot -include *.exe -recurse
+
+foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+if (($null -ne $Env:GITHUB_ACTIONS) -and ("" -ne $Env:GITHUB_ACTIONS)) {
+  Write-Host "::add-path::$binPackageDir"
+}
+
+if (($null -ne $Env:TRAVIS) -and ("" -ne $Env:TRAVIS)) {
+  Write-Host "Configuring Travis aliases."
+  # We need to fix up refreshenv for Travis
+  $rc = $Env:HOME + "/.bashrc"
+
+  Add-Content $rc "function refreshenv
+{
+  powershell -NonInteractive - <<\EOF
+Import-Module -Force `"`$env:ChocolateyInstall\helpers\chocolateyProfile.psm1`"
+`$pref = `$ErrorActionPreference
+`$ErrorActionPreference = 'SilentlyContinue'
+Update-SessionEnvironment
+`$ErrorActionPreference = `$pref
+# Round brackets in variable names cause problems with bash
+Get-ChildItem env:* | %{
+  if (!(`$_.Name.Contains('('))) {
+    `$value = `$_.Value
+    if (`$_.Name -eq 'PATH') {
+      `$value = `$value -replace ';',':'
+    }
+    Write-Output (`"export `" + `$_.Name + `"='`" + `$value + `"'`")
+  }
+} | Out-File -Encoding ascii $env:TEMP\refreshenv.sh
+EOF
+
+  source `"$env:TEMP/refreshenv.sh`"
+}
+
+alias RefreshEnv=refreshenv"
+
+  Write-Host "Ok, updated ~/.bashrc, source to use refreshenv."
+}
