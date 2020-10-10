51bdab4ed5b0a9981c0123c6bb3dfae1c1a5329b
Tamar Christina <tamar@zhox.com>
2020-10-10 11:52:31 +0100

    Add GHC 9.0.1 Alpha1

---
+++ 9.0.1-alpha1/ghc/tools/chocolateyInstall.ps1
@@ -0,0 +1,116 @@
+﻿$version     = '9.0.0.20200925'
+$packageName = 'ghc'
+$url         = 'https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-i386-unknown-mingw32.tar.xz'
+$binSha256   = '9c0d88165a9ea4b6307a01a356fc4dbf25b101aabe1694c576986f7a678e30f7'
+$variant     = ''
+$pp          = Get-PackageParameters
+if ($pp['integer-simple']) {
+  $variant = '-integer-simple'
+  $binSha256 = 'e12c4780444b4f32c413faf5212ff50d358fe159d2833786e72828e0b46909ad'
+}
+$baseTarget  = 'ghc-' + $version + '-x86_64-unknown-mingw32'
+$target      = $baseTarget + $variant
+$url64       = 'https://downloads.haskell.org/~ghc/9.0.1-alpha1/' + $target + '.tar.xz'
+$is32 = (Get-OSArchitectureWidth 32)  -or $env:chocolateyForceX86 -eq 'true'
+
+if($is32)
+  {
+      Write-Host "#     # ####### ####### ###  #####  #######"
+      Write-Host "##    # #     #    #     #  #     # #      "
+      Write-Host "# #   # #     #    #     #  #       #      "
+      Write-Host "#  #  # #     #    #     #  #       #####  "
+      Write-Host "#   # # #     #    #     #  #       #      "
+      Write-Host "#    ## #     #    #     #  #     # #      "
+      Write-Host "#     # #######    #    ###  #####  #######"
+      Write-Host ""
+      Write-Host " 32 bit binary for Windows is no longer supported."
+      Write-Host "GHC 8.6.5 will be installed instead."
+      Write-Host ""
+      # rewrite the version to 8.6.5 so installer works
+      $version = '8.6.5'
+  }
+
+# Fixes issue #4
+if ($pp['installdir']) {
+  $binRoot         = $pp['installdir']
+} else {
+  $binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+}
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+$tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+$tarFile = $packageName + "Install"
+$tarPath = Join-Path $tmpPath $tarFile
+$tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath `
+    -Url $url -ChecksumType sha256 -Checksum 2a8fb73080ed4335f7a172fe6cf9da1a2faa51fdb72817c50088292f497fc57a `
+    -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 $binSha256
+Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+rm $tmpFile # Clean up temporary file
+
+# new folders are stupidly long and verbose and break convention.
+# let's normalize them
+$longBinPackageDir = Join-Path $binRoot $baseTarget
+Rename-Item $longBinPackageDir $packageFullName
+
+# FIxes issue #8
+$installScope = 'User'
+if ($pp['globalinstall'] -eq 'true') {
+  $installScope = 'Machine'
+  Write-Host "Install scope set to global."
+}
+Install-ChocolateyPath "$binPackageDir" -Machine "$installScope"
+
+Write-Host "Hiding shims for `'$binRoot`'."
+$files = get-childitem $binRoot -include *.exe -recurse
+
+foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+# Detect Github Actions
+if (($null -ne $Env:GITHUB_ACTIONS) -and ("" -ne $Env:GITHUB_ACTIONS)) {
+  Write-Host "::add-path::$binPackageDir"
+}
+
+# Detect Azure Pipelines (Fixes issue #9)
+if (($null -ne $Env:TF_BUILD) -and ("" -ne $Env:TF_BUILD)) {
+  Write-Host "##vso[task.prependpath]$binPackageDir"
+}
+
+if (($null -ne $Env:TRAVIS) -and ("" -ne $Env:TRAVIS)) {
+  Write-Host "Configuring Travis aliases."
+  # We need to fix up refreshenv for Travis
+  $rc = $Env:HOME + "/.bashrc"
+
+  Add-Content $rc "function refreshenv
+{
+  powershell -NonInteractive - <<\EOF
+Import-Module -Force `"`$env:ChocolateyInstall\helpers\chocolateyProfile.psm1`"
+`$pref = `$ErrorActionPreference
+`$ErrorActionPreference = 'SilentlyContinue'
+Update-SessionEnvironment
+`$ErrorActionPreference = `$pref
+# Round brackets in variable names cause problems with bash
+Get-ChildItem env:* | %{
+  if (!(`$_.Name.Contains('('))) {
+    `$value = `$_.Value
+    if (`$_.Name -eq 'PATH') {
+      `$value = `$value -replace ';',':'
+    }
+    Write-Output (`"export `" + `$_.Name + `"='`" + `$value + `"'`")
+  }
+} | Out-File -Encoding ascii $env:TEMP\refreshenv.sh
+EOF
+
+  source `"$env:TEMP/refreshenv.sh`"
+}
+
+alias RefreshEnv=refreshenv"
+
+  Write-Host "Ok, updated ~/.bashrc, source to use refreshenv."
+}
