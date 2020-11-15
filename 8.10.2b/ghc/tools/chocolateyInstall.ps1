3630eb8fb521e238de241e23e2602c09d8882c0c
Tamar Christina <tamar@zhox.com>
2020-11-15 22:45:58 +0000

    Updated versions of packages which support github actions

---
+++ 8.10.2b/ghc/tools/chocolateyInstall.ps1
@@ -0,0 +1,112 @@
+﻿$version     = '8.10.2'
+$packageName = 'ghc'
+$url         = 'https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-i386-unknown-mingw32.tar.xz'
+$url64       = 'https://downloads.haskell.org/~ghc/8.10.2/ghc-8.10.2-x86_64-unknown-mingw32.tar.xz'
+
+$is32 = (Get-OSArchitectureWidth 32)  -or $env:chocolateyForceX86 -eq 'true'
+
+if($is32)
+  {
+      Write-Host "#     # ####### ####### ###  #####  #######"
+      Write-Host "##    # #     #    #     #  #     # #      "
+      Write-Host "# #   # #     #    #     #  #       #      "
+      Write-Host "#  #  # #     #    #     #  #       #####  "
+      Write-Host "#   # # #     #    #     #  #       #      "
+      Write-Host "#    ## #     #    #     #  #     # #      "
+      Write-Host "#     # #######    #    ###  #####  #######"
+      Write-Host ""
+      Write-Host " 32 bit binary for Windows is no longer supported."
+      Write-Host "GHC 8.6.5 will be installed instead."
+      Write-Host ""
+      # rewrite the version to 8.6.5 so installer works
+      $version = '8.6.5'
+  }
+
+$pp = Get-PackageParameters
+
+# Fixes issue #4
+if ($pp['installdir']) {
+  $binRoot         = $pp['installdir']
+} else {
+  $binRoot         = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+}
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+$tmpPath = Join-Path $env:chocolateyPackageFolder tmp
+$tarFile = $packageName + "Install"
+$tarPath = Join-Path $tmpPath $tarFile
+$tmpFile = Join-Path $binRoot ($tarFile + "~")
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $tarPath `
+    -Url $url -ChecksumType sha256 -Checksum 2a8fb73080ed4335f7a172fe6cf9da1a2faa51fdb72817c50088292f497fc57a `
+    -Url64bit $url64 -ChecksumType64 sha256 -Checksum64 dcae4c173b9896e07ff048de5509aa0a4537233150e06e5ce8848303dfadc176
+Get-ChocolateyUnzip -fileFullPath $tarPath -destination $binRoot
+Get-ChocolateyUnzip -fileFullPath $tmpFile -destination $binRoot
+rm $tmpFile # Clean up temporary file
+
+# FIxes issue #8
+$installScope = 'User'
+if ($pp['globalinstall'] -eq 'true') {
+  $installScope = 'Machine'
+  Write-Host "Install scope set to global."
+}
+Install-ChocolateyPath "$binPackageDir" -Machine "$installScope"
+
+Write-Host "Hiding shims for `'$binRoot`'."
+$files = get-childitem $binRoot -include *.exe -recurse
+
+foreach ($file in $files) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+# Detect Github Actions and use new method. (Fixes issue #10)
+if (($null -ne $Env:GITHUB_ACTIONS) -and ("" -ne $Env:GITHUB_ACTIONS)) {
+  echo "$binPackageDir" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+}
+
+# Detect Azure Pipelines (Fixes issue #9)
+if (($null -ne $Env:TF_BUILD) -and ("" -ne $Env:TF_BUILD)) {
+  Write-Host "##vso[task.prependpath]$binPackageDir"
+}
+
+if (($null -ne $Env:TRAVIS) -and ("" -ne $Env:TRAVIS)) {
+  Write-Host "Configuring Travis aliases."
+  # We need to fix up refreshenv for Travis
+  $rc = $Env:HOME + "/.bashrc"
+
+  Add-Content $rc "function refreshenv
+{
+  powershell -NonInteractive - <<\EOF
+Import-Module -Force `"`$env:ChocolateyInstall\helpers\chocolateyProfile.psm1`"
+`$pref = `$ErrorActionPreference
+`$ErrorActionPreference = 'SilentlyContinue'
+Update-SessionEnvironment
+`$ErrorActionPreference = `$pref
+# Round brackets in variable names cause problems with bash
+Get-ChildItem env:* | %{
+  if (!(`$_.Name.Contains('('))) {
+    `$value = `$_.Value
+    if (`$_.Name -eq 'PATH') {
+      `$value = `$value -replace ';',':'
+    }
+    Write-Output (`"export `" + `$_.Name + `"='`" + `$value + `"'`")
+  }
+} | Out-File -Encoding ascii $env:TEMP\refreshenv.sh
+EOF
+
+  source `"$env:TEMP/refreshenv.sh`"
+}
+
+alias RefreshEnv=refreshenv"
+
+  Write-Host "Ok, updated ~/.bashrc, source to use refreshenv."
+}
+
+# Fix broken GHC 8.10.2 package by replacing the hardcoded linker PATH with a working one.
+$brokenPath = "C:/GitLabRunner/builds/2WeHDSFP/0/ghc/ghc/inplace/mingw/bin/ld.exe"
+$correctPath = Join-Path $packageFullName "mingw/bin/ld.exe"
+$unixPath = $correctPath -replace "\\","/"
+$settingsFIle = Join-Path $packageFullName "lib/settings"
+((Get-Content -path $settingsFIle -Raw) -replace "$brokenPath","$unixPath") | Set-Content -Path $settingsFIle
