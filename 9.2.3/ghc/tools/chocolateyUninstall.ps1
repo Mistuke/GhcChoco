4950c8029dfc942cceb48361e530b4a4ab9e4466
Tamar Christina <tamar@zhox.com>
2022-05-28 12:05:52 +0100

    Add GHC 9.2.3

---
+++ 9.2.3/ghc/tools/chocolateyUninstall.ps1
@@ -0,0 +1,29 @@
+﻿$version     = '9.2.3'
+$packageName = 'ghc'
+$url         = 'https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-i386-unknown-mingw32.tar.xz'
+$binSha256   = '4f4ab118df01cbc7e7c510096deca0cb25025339a97730de0466416296202493'
+$variant     = ''
+$pp          = Get-PackageParameters
+if ($pp['integer-simple']) {
+  $variant = '-integer-simple'
+  $binSha256 = '7edc8d07b07e095cd21c18ddd9d7f7a3ba30d02c7c82381edd392807589a2678'
+}
+$baseTarget  = 'ghc-' + $version + '-x86_64-unknown-mingw32'
+$target      = $baseTarget + $variant
+$url64       = 'https://downloads.haskell.org/~ghc/9.2.3/' + $target + '.tar.xz'
+$is32 = (Get-OSArchitectureWidth 32)  -or $env:chocolateyForceX86 -eq 'true'
+
+# Fixes issue #4
+if ($pp['installdir']) {
+  $binRoot         = $pp['installdir']
+} else {
+  $binRoot         = Get-ToolsLocation
+}
+$packageFullName = Join-Path $binRoot ($packageName + '-' + $version)
+$binPackageDir   = Join-Path $packageFullName "bin"
+
+if (-Not $is32) {
+
+}
+
+Remove-Item -Force -Recurse $packageFullName
